6894ed02fe01b0e19fff9676fe85538fc16c5c11
jairH3110 <chiefeon3110@gmail.com>
2023-04-18 14:15:28 -0600

    lol

--- settings/production.py
+++ settings/production.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for TESTuction
+# Quick-start development settings s- unsuitable for TESTuction
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in TESTuction secret!
